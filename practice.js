c6c63f8825776352a830fac5a3c208fc1bbe20b7
yeexng <118996751+yeexng@users.noreply.github.com>
2022-11-28 14:40:25 +0000

    Intro JS
    Brief Intro to JS by adding info of mine.

---
+++ practice.js
@@ -0,0 +1,10 @@
+let firstName = "YeeXuan";
+let lastName = "Ng"
+
+console.log ("Hello, my name is", firstName, lastName)
+
+let age = 26;
+let currentYear = 2022;
+let birthYear = currentYear - age;
+
+console.log (birthYear)
